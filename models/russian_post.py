fa0f6555dc1d47cbaf716cf1f1f7f8d0181f1c8b
romeotestuser <romeo.abulencia@gmail.com>
2017-12-07 01:33:48 +0800

    Added notes.

--- models/russian_post.py
+++ models/russian_post.py
@@ -421,6 +421,20 @@ class generate_delivery_form(models.TransientModel):
                     }
                     #pbrf.ru api execution
                     #sample: https://pbrf.ru/%D0%B4%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F/%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80-%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81%D0%B0
+                    #todo:
+                    #proper data generation of the following data keys:
+
+#                     index_from
+#                     bik
+#                     current_account
+#                     MailCtg
+#                     message
+#                     letter_to
+#                     tracking_number
+#                     from_apartment
+#                     corpus_to
+#                     slash_to
+#                     mail_count
                     import urllib
                     import json
                     params={}    
